1922ab1319511f7223dd9644c4e0f78bc6144e72
Edwin Cruz <ecruz1113@gmail.com>
2021-02-16 23:16:20 -0500

    Add necessary constants to settings
    Adds which user model to use for authentication, the url to redirect to
    after log in, the media root and url, and the template pack to use for
    crispy forms.

--- PoshBot/settings.py
+++ PoshBot/settings.py
@@ -123,3 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Crispy Forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Custom User & Log In
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = 'home'
