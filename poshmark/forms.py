c40a686668d44f95871979834a266fa2731d5292
Edwin Cruz <ecruz1113@gmail.com>
2021-02-19 16:01:17 -0500

    Add clean and save functions for CreatePoshUser

--- poshmark/forms.py
+++ poshmark/forms.py
@@ -9,3 +9,33 @@ class CreatePoshUser(forms.ModelForm):
     class Meta:
         model = PoshUser
         fields = ['profile_picture', 'first_name', 'last_name', 'email', 'username', 'password', 'gender']
+
+    def clean(self):
+        if self.cleaned_data['alias'] == '1':
+            tmp_user = PoshUser()
+            can_create_alias = tmp_user.check_alias_email()
+
+            if not can_create_alias:
+                self.add_error('email', 'The limit of email aliases have been met, cannot create more.')
+
+    def save(self, commit=True):
+        new_user = super(CreatePoshUser, self).save(commit=False)
+
+        if self.cleaned_data['alias'] == '1':
+            masked_email = self.cleaned_data['email']
+            alias = new_user.generate_email(masked_email)
+            new_user.email = alias.email_address
+            new_user.is_email_verified = alias.is_verified
+            new_user.alias_email_id = alias.id
+            new_user.masked_email = alias.masked_email_address
+
+            if alias.is_verified:
+                new_user.status = '4'
+            else:
+                new_user.status = '3'
+
+        else:
+            new_user.email = self.cleaned_data['email']
+            new_user.is_email_verified = True
+
+        new_user.save()
