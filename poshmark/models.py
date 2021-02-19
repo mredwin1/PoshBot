c03b3eedc0f8bb58d164ecdfccce3e3c26e44516
Edwin Cruz <ecruz1113@gmail.com>
2021-02-19 15:55:03 -0500

    Add masked_email and alias_email_id field
    These will allow for me to show the masked_email to the end user for a
    specific PoshUser and will also allow me to delete the associated email
    alias when the PoshUser gets deleted.

--- poshmark/models.py
+++ poshmark/models.py
@@ -39,6 +39,8 @@ class PoshUser(models.Model):
     last_name = models.CharField(max_length=30)
     email = models.EmailField(help_text="If alias is chosen up top then put the email you wish to mask here. Otherwise "
                                         "put the email you wish to create the Posh User with.")
+    masked_email = models.EmailField(default="")
+    alias_email_id = models.CharField(max_length=100, default="")
     username = models.CharField(max_length=20, unique=True)
     password = models.CharField(max_length=20)
     gender = models.CharField(max_length=20, choices=GENDER_CHOICES)
