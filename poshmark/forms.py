5d531c652d5d0cb780ba1cc67f6064b0f8d472d8
Edwin Cruz <ecruz1113@gmail.com>
2021-02-19 16:00:54 -0500

    Add alias field to form
    This is just to have somewhere to store the value so the form does not
    return that it isn't valid.

--- poshmark/forms.py
+++ poshmark/forms.py
@@ -3,6 +3,9 @@ from poshmark.models import PoshUser
 
 
 class CreatePoshUser(forms.ModelForm):
+    alias_choices = [('1', 'Alias'), ('0', 'No Alias')]
+    alias = forms.CharField(max_length=10, label=None, widget=forms.RadioSelect(choices=alias_choices))
+
     class Meta:
         model = PoshUser
         fields = ['profile_picture', 'first_name', 'last_name', 'email', 'username', 'password', 'gender']
