77003d4c913a43285aacb7d11220a537b0d8365b
Edwin Cruz <ecruz1113@gmail.com>
2021-02-26 18:21:58 -0500

    Add more logging

--- poshmark/poshmark_client/poshmark_client.py
+++ poshmark/poshmark_client/poshmark_client.py
@@ -3,6 +3,7 @@ import random
 import re
 import requests
 import time
+import traceback
 
 from pathlib import Path
 from selenium import webdriver
@@ -123,6 +124,7 @@ class PoshMarkClient:
         return True
 
     def check_for_errors(self):
+        self.logger.info('Checking for errors')
         captcha_errors = [
             'Invalid captcha',
             'Please enter your login information and complete the captcha to continue.'
@@ -136,7 +138,7 @@ class PoshMarkClient:
         elif banner_error:
             error = self.locate(By.CLASS_NAME, 'error_banner')
         else:
-            self.logger.error('No known errors were encountered')
+            self.logger.info('No known errors were encountered')
             return None
 
         if self.is_present(By.CLASS_NAME, 'base_error_message') or self.is_present(By.CLASS_NAME, 'error_banner'):
@@ -148,7 +150,7 @@ class PoshMarkClient:
                 return 'ERROR_USERNAME_PASSWORD'
 
             elif error.text in captcha_errors:
-                self.logger.info('Captcha encountered')
+                self.logger.warning('Captcha encountered')
                 captcha_iframe = self.locate(By.TAG_NAME, 'iframe', location_type='visibility')
                 captcha_src = captcha_iframe.get_attribute('src')
                 google_key = re.findall(r'(?<=k=)(.*?)(?=&)', captcha_src)[0]
@@ -158,7 +160,7 @@ class PoshMarkClient:
                 retries = 1
 
                 while captcha_response is None and retries != 5:
-                    self.logger.info('Retrying captcha again...')
+                    self.logger.warning('Captcha not solved. Retrying captcha again...')
                     captcha_response = captcha_solver.solve_captcha()
                     retries += 1
 
@@ -167,7 +169,8 @@ class PoshMarkClient:
                 elif captcha_response == -1:
                     self.logger.error('Exiting after encountering an error with the captcha.')
                 else:
-                    self.logger.info(f'2Captcha successfully solved captcha after {retries} attempts')
+                    word = 'attempt' if retries == 1 else 'attempts'
+                    self.logger.info(f'2Captcha successfully solved captcha after {retries} {word}')
                     # Set the captcha response
                     self.web_driver.execute_script(
                         f'document.getElementById("g-recaptcha-response").innerHTML="{captcha_response}";')
@@ -175,7 +178,7 @@ class PoshMarkClient:
                 return 'CAPTCHA'
 
     def register(self):
-        if self.posh_user.is_signed_up:
+        if self.posh_user.is_registered:
             pass
         else:
             previous_status = self.posh_user.status
@@ -185,12 +188,15 @@ class PoshMarkClient:
                 self.posh_user.save()
                 # Start at home page so it is more realistic
                 self.web_driver.get('https://poshmark.com')
+                self.logger.info(f'At poshmark homepage - {self.web_driver.current_url}')
+                self.logger.info('Locating sign up button')
 
                 # Random wait for more realism
                 time.sleep(random.randint(1, 5))
 
                 # Pick one of the two signup buttons for more randomness and click it
-                sign_up_button_xpath = random.choice(['//a[@href="/signup?"]', '//a[@href="/signup"]'])
+                sign_up_button_xpath = random.choice(['//*[@id="content"]/div/div[1]/div/div[2]/div/div/a',
+                                                      '//*[@id="app"]/header/nav/div/div/a[2]'])
                 signup_button = self.locate(By.XPATH, sign_up_button_xpath)
                 signup_button.click()
                 time.sleep(random.randint(1, 4))
@@ -204,6 +210,7 @@ class PoshMarkClient:
                 gender_field = self.locate(By.CLASS_NAME, 'dropdown__selector--select-tag')
 
                 # Send keys and select gender
+                self.logger.info('Filling out form')
                 first_name_field.send_keys(self.posh_user.first_name)
                 time.sleep(random.randint(1, 2))
                 last_name_field.send_keys(self.posh_user.last_name)
@@ -228,60 +235,91 @@ class PoshMarkClient:
                 # Submit the form
                 done_button.click()
 
+                self.logger.info('Form submitted')
+
                 error_code = self.check_for_errors()
                 if error_code == 'CAPTCHA':
                     done_button = self.locate(By.XPATH, '//button[@type="submit"]')
                     done_button.click()
+                    self.logger.info('Resubmitted form after entering captcha')
 
-                self.posh_user.status = '1'
-                self.posh_user.save()
+                # Sleep for realism
+                self.sleep(1, 2)
 
-                # Next Section - Profile
-                time.sleep(random.randint(1, 3))  # Sleep for realism
-                next_button = self.locate(By.XPATH, '//button[@type="submit"]')
-                next_button.click()
-                self.posh_user.is_registered = True
-                self.posh_user.save()
+                # Check if Posh User is now registered
+                response = requests.get(f'https://poshmark.com/closet/{self.posh_user.username}')
+                user_image_located = self.locate(By.CLASS_NAME, 'user-image')
 
-                # Next Section - Select Brands (will not select brands)
-                time.sleep(random.randint(1, 3))  # Sleep for realism
-                brands = self.web_driver.find_elements_by_class_name('content-grid-item')
-                next_button = self.locate(By.XPATH, '//button[@type="submit"]')
+                if user_image_located and response.status_code == requests.codes.ok:
+                    self.posh_user.is_registered = True
+                    self.posh_user.status = '1'
+                    self.posh_user.save()
+                    self.logger.info(f'Successfully registered {self.posh_user.username}, status changed to "Active"')
 
-                # Select random brands then click next
-                for x in range(random.randint(3, 5)):
-                    try:
-                        brand = random.choice(brands)
-                        brand.click()
-                    except IndexError:
-                        pass
-                next_button.click()
+                    # Next Section - Profile
+                    next_button = self.locate(By.XPATH, '//button[@type="submit"]')
+                    next_button.click()
 
-                # Next Section - All Done Page
-                time.sleep(random.randint(1, 2))  # Sleep for realism
-                start_shopping_button = self.locate(By.XPATH, '//button[@type="submit"]')
-                start_shopping_button.click()
-            except:
+                    # Next Section - Select Brands (will not select brands)
+                    self.sleep(1, 3)  # Sleep for realism
+                    self.logger.info('Selecting random brands')
+                    brands = self.web_driver.find_elements_by_class_name('content-grid-item')
+                    next_button = self.locate(By.XPATH, '//button[@type="submit"]')
+
+                    # Select random brands then click next
+                    for x in range(random.randint(3, 5)):
+                        try:
+                            brand = random.choice(brands)
+                            brand.click()
+                        except IndexError:
+                            pass
+                    next_button.click()
+
+                    # Next Section - All Done Page
+                    self.sleep(1, 3)  # Sleep for realism
+                    start_shopping_button = self.locate(By.XPATH, '//button[@type="submit"]')
+                    start_shopping_button.click()
+                    self.logger.info('Registration Complete')
+                else:
+                    self.posh_user.status = '4'
+                    self.posh_user.save()
+                    if not user_image_located:
+                        self.logger.error(f'Registration for {self.posh_user.username} unsuccessful (form not '
+                                          f'submitted)')
+                    elif response.status_code != requests.codes.ok:
+                        self.logger.error(f'Registration for {self.posh_user.username} unsuccessful (could not find '
+                                          f'user closet at {response.url})')
+                    self.logger.error('Status changed to "Waiting to be registered"')
+            except Exception as e:
+                self.logger.error(f'Error encountered - Changing status back to {previous_status}')
+                self.logger.error(f'{traceback.format_exc()}')
+
                 self.posh_user.status = previous_status
+                self.posh_user.save()
 
     def log_in(self):
         """Will go to the Posh Mark home page and log in using waits for realism"""
         self.logger.info(f'Logging {self.posh_user.username} in')
         if not self.web_driver.current_url == 'https://poshmark.com/':
             self.web_driver.get('https://poshmark.com/')
-            time.sleep(random.randint(1, 3))
-
+            self.sleep(1, 3)
+        self.logger.info(f'At poshmark homepage - {self.web_driver.current_url}')
+        self.logger.info(f'locating login button')
         log_in_nav = self.locate(By.XPATH, '//a[@href="/login"]')
         log_in_nav.click()
+        self.logger.info(f'Clicked login button - Current URL: {self.web_driver.current_url}')
 
         time.sleep(random.randint(1, 3))
         username_field = self.locate(By.ID, 'login_form_username_email')
         password_field = self.locate(By.ID, 'login_form_password')
 
+        self.logger.info('Filling in form')
+
         username_field.send_keys(self.posh_user.username)
         time.sleep(random.randint(1, 2))
         password_field.send_keys(self.posh_user.password)
         password_field.send_keys(Keys.RETURN)
+        self.logger.info('Form submitted')
 
         error_code = self.check_for_errors()
 
@@ -290,13 +328,118 @@ class PoshMarkClient:
             time.sleep(1)
             password_field.send_keys(self.posh_user.password)
             password_field.send_keys(Keys.RETURN)
+            self.logger.info('Form resubmitted')
 
     def check_logged_in(self):
         """Will go to poshmark.com to see if the PoshUser is logged in or not, the bot knows if they are logged in if it
         can find the search bar which is only displayed in the feed page when someone is logged in"""
+        self.logger.info('Checking if user is signed in')
         self.web_driver.get('https://poshmark.com')
+        result = self.is_present(By.ID, 'searchInput')
+        if result:
+            self.logger.info('User is logged in')
+        else:
+            self.logger.info('User is not logged in')
 
-        return self.is_present(By.ID, 'searchInput')
+        return result
+
+    def update_profile(self):
+        previous_status = self.posh_user.status
+        try:
+            self.logger.info('Updating Profile')
+            self.posh_user.status = '6'
+            self.posh_user.save()
+
+            if not self.check_logged_in():
+                self.log_in()
+
+            time.sleep(random.randint(1, 3))
+
+            profile_dropdown = self.locate(By.XPATH, '//*[@id="app"]/header/nav[1]/div/ul/li[5]/div/div[1]/div')
+            profile_dropdown.click()
+
+            self.logger.info('Clicked profile dropdown')
+
+            self.sleep(1)
+
+            my_closet_button = self.locate(By.XPATH, f'//a[@href="/closet/{self.posh_user.username}"]')
+            my_closet_button.click()
+
+            self.logger.info('Clicked my closet button')
+
+            self.sleep(1, 3)
+
+            edit_profile_button = self.locate(By.XPATH, '//a[@href="/user/edit-profile"]')
+            edit_profile_button.click()
+
+            self.logger.info('Clicked on edit profile button')
+
+            self.sleep(1, 3)
+
+            # This while is to ensure that the profile picture path exists and tries 5 times
+            attempts = 1
+            profile_picture_path = self.posh_user.profile_picture.path
+            profile_picture_exists = Path(profile_picture_path).is_file()
+            while not profile_picture_exists and attempts < 6:
+                self.logger.info(str(profile_picture_path))
+                self.logger.warning(f'Could not find profile picture file. Attempt # {attempts}')
+                self.sleep(5)
+                profile_picture_exists = Path(profile_picture_path).is_file()
+                attempts += 1
+            else:
+                if not profile_picture_exists:
+                    self.logger.error('Could not upload profile picture - Picture not found.')
+                else:
+                    profile_picture = self.locate(By.XPATH, '//*[@id="content"]/div/div[2]/div/div[1]/div[3]/label/input')
+                    profile_picture.send_keys(profile_picture_path)
+
+                    apply_button = self.locate(
+                        By.XPATH, '//*[@id="content"]/div/div[2]/div/div[1]/div[3]/div/div[2]/div[2]/div/button[2]')
+                    apply_button.click()
+
+                    self.logger.info('Profile picture uploaded')
+
+                    self.sleep(1)
+
+            attempts = 1
+            header_picture_path = self.posh_user.profile_picture.path
+            header_picture_exists = Path(header_picture_path).is_file()
+            while not header_picture_exists and attempts < 6:
+                self.logger.info(str(header_picture_path))
+                self.logger.warning(f'Could not find header picture file. Attempt # {attempts}')
+                self.sleep(5)
+                header_picture_exists = Path(header_picture_path).is_file()
+                attempts += 1
+            else:
+                if not header_picture_exists:
+                    self.logger.error('Could not upload header picture - Picture not found')
+                else:
+                    header_picture = self.locate(By.XPATH, '//*[@id="content"]/div/div[2]/div/div[1]/div[2]/label/input')
+                    header_picture.send_keys(header_picture_path)
+
+                    apply_button = self.locate(
+                        By.XPATH, '//*[@id="content"]/div/div[2]/div/div[1]/div[2]/div/div[2]/div[2]/div/button[2]')
+                    apply_button.click()
+
+                    self.logger.info('Header picture uploaded')
+
+                    self.sleep(1)
+
+            save_button = self.locate(By.CLASS_NAME, 'btn--primary')
+            save_button.click()
+
+            self.logger.info('Profile saved')
+
+            self.posh_user.status = '1'
+            self.posh_user.save()
+
+            self.logger.info('Posh User status changed to "Active"')
+        except Exception as e:
+            self.logger.error(f'Error encountered - Changing status back to {previous_status}')
+            self.logger.error(f'{traceback.format_exc()}')
+
+            self.posh_user.status = previous_status
+            self.posh_user.save()
 
     def check_news(self):
         """If the PoshUser is logged in it will check their new else it will log them in then check their news"""
@@ -315,82 +458,3 @@ class PoshMarkClient:
         else:
             offers_nav = self.locate(By.XPATH, '//a[@href="/offers/my_offers"]')
             offers_nav.click()
-
-    def update_profile(self):
-        previous_status = self.posh_user.status
-        try:
-            self.logger.info('Updating Profile')
-            self.posh_user.status = '6'
-            self.posh_user.save()
-
-            if not self.check_logged_in():
-                self.log_in()
-
-            time.sleep(random.randint(1, 3))
-
-            profile_dropdown = self.locate(By.XPATH, '//*[@id="app"]/header/nav[1]/div/ul/li[5]/div/div[1]/div')
-            profile_dropdown.click()
-
-            time.sleep(1)
-
-            my_closet_button = self.locate(By.XPATH, f'//a[@href="/closet/{self.posh_user.username}"]')
-            my_closet_button.click()
-
-            time.sleep(random.randint(1, 3))
-
-            edit_profile_button = self.locate(By.XPATH, '//a[@href="/user/edit-profile"]')
-            edit_profile_button.click()
-
-            time.sleep(random.randint(1, 3))
-
-            # This while is to ensure that the profile picture path exists and tries 5 times
-            attempts = 1
-            profile_picture_path = self.posh_user.profile_picture.path
-            profile_picture_exists = Path(profile_picture_path).is_file()
-            while not profile_picture_exists and attempts < 6:
-                time.sleep(5)
-                profile_picture_exists = Path(profile_picture_path).is_file()
-                attempts += 1
-            else:
-                if attempts < 6:
-                    self.logger.error('Could not upload profile picture - Picture not found')
-                else:
-                    profile_picture = self.locate(By.XPATH, '//*[@id="content"]/div/div[2]/div/div[1]/div[3]/label/input')
-                    profile_picture.send_keys(profile_picture_path)
-
-                    time.sleep(1)
-
-                    apply_button = self.locate(
-                        By.XPATH, '//*[@id="content"]/div/div[2]/div/div[1]/div[3]/div/div[2]/div[2]/div/button[2]')
-                    apply_button.click()
-
-                    time.sleep(1)
-
-            attempts = 1
-            header_picture_path = self.posh_user.profile_picture.path
-            header_picture_exists = Path(header_picture_path).is_file()
-            while not header_picture_exists and attempts < 6:
-                time.sleep(5)
-                header_picture_exists = Path(header_picture_path).is_file()
-                attempts += 1
-            else:
-                if attempts < 6:
-                    self.logger.error('Could not upload header picture - Picture not found')
-                else:
-                    header_picture = self.locate(By.XPATH, '//*[@id="content"]/div/div[2]/div/div[1]/div[2]/label/input')
-                    header_picture.send_keys(header_picture_path)
-
-                    time.sleep(1)
-
-                    apply_button = self.locate(
-                        By.XPATH, '//*[@id="content"]/div/div[2]/div/div[1]/div[2]/div/div[2]/div[2]/div/button[2]')
-                    apply_button.click()
-                    time.sleep(1)
-
-            save_button = self.locate(By.CLASS_NAME, 'btn--primary')
-            save_button.click()
-
-            self.posh_user.status = '1'
-            self.posh_user.save()
-        except:
-            self.posh_user.status = previous_status
