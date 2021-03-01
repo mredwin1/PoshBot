623e8aafdd14893b1582deb611f13d014b291c0e
Edwin Cruz <ecruz1113@gmail.com>
2021-03-01 16:53:15 -0500

    Change tasks to use the Log models as loggers

--- poshmark/tasks.py
+++ poshmark/tasks.py
@@ -1,14 +1,13 @@
-import logging
-
-from .models import PoshUser
+from .models import PoshUser, Log
 from celery import shared_task
 from poshmark.poshmark_client.poshmark_client import PoshMarkClient
 
 
 @shared_task
 def register_posh_user(posh_user_id):
-    logger = logging.getLogger(__name__)
+    """Registers a PoshUser to poshmark"""
     posh_user = PoshUser.objects.get(id=posh_user_id)
+    logger = Log(logger_type='1', posh_user=posh_user)
 
     with PoshMarkClient(posh_user, logger) as client:
         client.register()
@@ -17,11 +16,8 @@ def register_posh_user(posh_user_id):
 
 @shared_task
 def check_registered_posh_users():
+    """Will get all unregistered users and queue them up to be registered"""
     posh_users = PoshUser.objects.filter(is_registered=False)
 
     for posh_user in posh_users:
-        logger = logging.getLogger(__name__)
-
-        with PoshMarkClient(posh_user, logger) as client:
-            client.register()
-            client.update_profile()
+        register_posh_user(posh_user.id)
