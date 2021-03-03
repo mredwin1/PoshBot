014fabd7bb680de5e096de47d97c71516ab5c840
Edwin Cruz <ecruz1113@gmail.com>
2021-03-03 16:24:15 -0500

    Add log_cleanup task

--- poshmark/tasks.py
+++ poshmark/tasks.py
@@ -1,5 +1,8 @@
-from .models import PoshUser, Log
+from datetime import timedelta
+from django.utils import timezone
 from celery import shared_task
+
+from .models import PoshUser, Log
 from poshmark.poshmark_client.poshmark_client import PoshMarkClient
 
 
@@ -8,6 +11,7 @@ def register_posh_user(posh_user_id):
     """Registers a PoshUser to poshmark"""
     posh_user = PoshUser.objects.get(id=posh_user_id)
     logger = Log(logger_type='1', posh_user=posh_user)
+    logger.save()
 
     with PoshMarkClient(posh_user, logger) as client:
         client.register()
@@ -21,3 +25,11 @@ def check_registered_posh_users():
 
     for posh_user in posh_users:
         register_posh_user(posh_user.id)
+
+
+@shared_task
+def log_cleanup():
+    logs = Log.objects.filter(created__lte=timezone.now()-timedelta(days=2))
+
+    for log in logs:
+        log.delete()
