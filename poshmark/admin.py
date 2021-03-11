c4f5d8e5e525f36c27dcade037ccfe12df47a376
Edwin Cruz <ecruz1113@gmail.com>
2021-03-11 16:57:27 -0500

    Register Campaign to admin

--- poshmark/admin.py
+++ poshmark/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import PoshUser, Log, Listing
+from .models import PoshUser, Log, Listing, Campaign
 
 
 class PoshUserAdmin(admin.ModelAdmin):
@@ -30,3 +30,4 @@ class PoshUserAdmin(admin.ModelAdmin):
 admin.site.register(PoshUser, PoshUserAdmin)
 admin.site.register(Log)
 admin.site.register(Listing)
+admin.site.register(Campaign)
