b10de3c7df386fa636aa9075ee3fab202ba745cf
Edwin Cruz <ecruz1113@gmail.com>
2021-03-30 13:54:28 -0400

    JQuery for basic campaign form

--- poshmark/static/poshmark/JS/view_campaign_utilities.js
+++ poshmark/static/poshmark/JS/view_campaign_utilities.js
@@ -1,4 +1,63 @@
 $(document).ready(function () {
+    $.fn.inputFilter = function(inputFilter) {
+        return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+          if (inputFilter(this.value)) {
+            this.oldValue = this.value;
+            this.oldSelectionStart = this.selectionStart;
+            this.oldSelectionEnd = this.selectionEnd;
+          } else if (this.hasOwnProperty("oldValue")) {
+            this.value = this.oldValue;
+            this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+          } else {
+            this.value = "";
+          }
+        });
+    };
+    $("#id_delay").inputFilter(function(value) {
+        return /(^\d*$)|(\.$)|(^\d*\.\d*$)/.test(value);    // Allow digits only, using a RegExp
+    });
+    $('#addBasicCampaign').click(function () {
+        let modal = $('#mainModal');
+        let title = $('#mainModalTitle');
+
+        modal.modal('show');
+        title.text('Add Basic Campaign');
+    });
+    $('#basicCampaignForm').submit(function (event) {
+        event.preventDefault();
+
+        let form = $(this);
+        let main_modal = $('#mainModal');
+        let data = new FormData(form.get(0));
+
+        $.ajax({
+           url: form.attr('action'),
+           type: form.attr('method'),
+           data: data,
+           cache: false,
+           processData: false,
+           contentType: false,
+           success: function (data) {
+                main_modal.modal('hide');
+                location.reload()
+           },
+           error: function (data) {
+                $.each(data.responseJSON, function (key, value) {
+                    var id = '#id_' + key;
+                    var parent = $(id).parent();
+                    var p = $("<p>", {id: "error_1_id_" + key, "class": "invalid-feedback"});
+                    var strong = $("<strong>").text(value);
+
+                    parent.find('p').remove();
+                    p.append(strong);
+                    parent.append(p);
+                    p.show();
+
+
+                });
+           }
+        });
+    });
     $('.start').click(function () {
         let start_button = $(this);
         let stop_button = start_button.parent().parent().children().eq(1).children().eq(0);
@@ -45,5 +104,5 @@ $(document).ready(function () {
             },
         });
         return false;
-    })
+    });
 });
