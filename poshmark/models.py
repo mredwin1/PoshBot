109598b5037ff54b1e7023092ca7a2cc65eb67ee
Edwin Cruz <ecruz1113@gmail.com>
2021-04-02 14:04:49 -0400

    Move proxy port creation to a model function

--- poshmark/models.py
+++ poshmark/models.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import mailslurp_client
 import names
@@ -129,6 +130,60 @@ class PoshUser(models.Model):
                 logging.error(f'Exception when calling AliasControllerApi->create_alias {e}\n')
 
             return api_response
+
+    def generate_proxy_port(self, logger):
+        data = {
+            'zone':
+                {
+                    'name': self.username
+                },
+            'plan':
+                {
+                    'type': 'static',
+                    'pool_ip_type': 'static_res',
+                    'ip_fallback': 1,
+                    'ips_type': 'shared',
+                    'ips': 1,
+                    'country': 'us'
+                },
+            'ips': ['any'],
+            'perm': 'country'
+        }
+
+        headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {os.environ["PROXY_API_KEY"]}'}
+
+        zone_response = requests.post('https://luminati.io/api/zone', data=json.dumps(data), headers=headers)
+        zone_response_json = zone_response.json()
+
+        if zone_response.status_code != requests.codes.ok:
+            logger.critical('Zone could not be created - Not registering')
+        else:
+            last_user = PoshUser.objects.all().order_by('proxy_port').last()
+            last_port = last_user.proxy_port if last_user else None
+            headers = {'Content-Type': 'application/json'}
+            data = {
+                'proxy':
+                    {
+                        'port': last_port + 1 if last_port else 24000,
+                        'zone': self.username,
+                        'proxy_type': 'persist',
+                        'customer': os.environ['PROXY_CUSTOMER'],
+                        'password': zone_response_json['zone']['password'][0],
+                        'whitelist_ips': ["0.0.0.0/0"],
+                        'country': 'us',
+                    }
+            }
+            port_response = requests.post('http://lpm:22999/api/proxies', data=json.dumps(data), headers=headers)
+            port_response_json = port_response.json()
+            if 'errors' in port_response_json.keys():
+                logger.critical('The following errors encountered while creating a port')
+                for error in port_response_json['errors']:
+                    logger.critical(f"{error['msg']}")
+            else:
+                port = port_response_json['data']['port']
+                self.proxy_port = port
+
+            self.save()
 
     def delete_alias_email(self):
         """Using the mailslurp client it deletes it's alias email"""
