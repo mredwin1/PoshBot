dad418681bd1d27a57eb3043d593f51e795af3e9
Edwin Cruz <55803329+mredwin1@users.noreply.github.com>
2021-04-16 18:06:44 -0400

    Remove image disabling
    This is no longer needed since we are not using the old proxy

--- poshmark/poshmark_client/poshmark_client.py
+++ poshmark/poshmark_client/poshmark_client.py
@@ -98,7 +98,6 @@ class PoshMarkClient:
         self.web_driver_options = Options()
         self.web_driver_options.add_experimental_option("excludeSwitches", ["enable-automation"])
         self.web_driver_options.add_experimental_option('useAutomationExtension', False)
-        self.web_driver_options.add_experimental_option('prefs', {"profile.managed_default_content_settings.images": 2})
         self.web_driver_options.add_argument('--disable-extensions')
         self.web_driver_options.add_argument('--headless')
         self.web_driver_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
