b4503261d275386fc596b5d61a964b2d813f8cff
edwin.cruz <ecruz1113@gmail.com>
2021-07-09 13:52:08 -0400

    Update

--- poshmark/poshmark_client/poshmark_client.py
+++ poshmark/poshmark_client/poshmark_client.py
@@ -1502,7 +1502,7 @@ class PoshMarkClient:
                                 cleaned_comment = regex.sub('', text)
 
                                 if any([bad_word in cleaned_comment for bad_word in bad_words]):
-                                    report_button = text.lower().find_element_by_class_name('flag')
+                                    report_button = comment.find_element_by_class_name('flag')
                                     report_button.click()
 
                                     self.sleep(1)
