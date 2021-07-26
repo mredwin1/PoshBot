92dd4255f9500bd139b9ad48387e53adda6b9cc8
Edwin Cruz <ecruz1113@gmail.com>
2021-07-26 17:23:37 -0400

    Update

--- poshmark/models.py
+++ poshmark/models.py
@@ -95,10 +95,6 @@ class PoshUser(models.Model):
             '11': 'November',
             '12': 'December',
         }
-        user_info = {
-            'first_name': '',
-            'last_name': '',
-        }
         headers = {
             'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36'
         }
@@ -113,33 +109,38 @@ class PoshUser(models.Model):
         results = []
 
         try:
-            while not PoshUser.is_english(user_info['first_name'] + user_info['last_name']):
-                user_response = requests.get(user_url, params=user_payload, timeout=10, headers=headers)
-                response_results = user_response.json()['results']
-                for response_dict in response_results:
-                    header_image_response = requests.get(header_image_url, timeout=10, headers=headers)
-                    username = response_dict['login']['username']
-                    user_info = {
-                        'first_name': response_dict['name']['first'],
-                        'last_name': response_dict['name']['last'],
-                        'gender': response_dict['gender'].capitalize(),
-                        'email': f'{response_dict["email"][:-12]}',
-                        'username': username if len(username) <= 12 else username[:12],
-                        'password': response_dict['login']['password'],
-                        'dob_month': months[response_dict['dob']['date'][5:7]],
-                        'dob_day': response_dict['dob']['date'][8:10],
-                        'dob_year': response_dict['dob']['date'][:4],
-                        'profile_picture': response_dict['picture']['large'],
-                        'header_picture': header_image_response.url,
-                    }
+            user_response = requests.get(user_url, params=user_payload, timeout=10, headers=headers)
+            response_results = user_response.json()['results']
+            for response_dict in response_results:
+                header_image_response = requests.get(header_image_url, timeout=10, headers=headers)
+                username = response_dict['login']['username']
+                user_info = {
+                    'first_name': response_dict['name']['first'],
+                    'last_name': response_dict['name']['last'],
+                    'gender': response_dict['gender'].capitalize(),
+                    'email': f'{response_dict["email"][:-12]}',
+                    'username': username if len(username) <= 12 else username[:12],
+                    'password': response_dict['login']['password'],
+                    'dob_month': months[response_dict['dob']['date'][5:7]],
+                    'dob_day': response_dict['dob']['date'][8:10],
+                    'dob_year': response_dict['dob']['date'][:4],
+                    'profile_picture': response_dict['picture']['large'],
+                    'header_picture': header_image_response.url,
+                }
 
+                posh_user = PoshUser.objects.filter(username=user_info['username'])
+
+                while posh_user:
+                    user_info["username"] = PoshUser.generate_username(user_info['first_name'], user_info['last_name'])
+                    posh_user = PoshUser.objects.filter(username=user_info['username'])
+
+                username_test = requests.get(f'https://poshmark.com/closet/{user_info["username"]}', timeout=10)
+
+                while username_test.status_code == requests.codes.ok:
+                    user_info["username"] = PoshUser.generate_username(user_info['first_name'], user_info['last_name'])
                     username_test = requests.get(f'https://poshmark.com/closet/{user_info["username"]}', timeout=10)
 
-                    while username_test.status_code == requests.codes.ok:
-                        user_info["username"] = PoshUser.generate_username(user_info['first_name'], user_info['last_name'])
-                        username_test = requests.get(f'https://poshmark.com/closet/{user_info["username"]}', timeout=10)
-
-                    results.append(user_info)
+                results.append(user_info)
         except requests.exceptions.RequestException as e:
             logging.error('Error occurred while trying to get profiles')
 
@@ -196,17 +197,6 @@ class PoshUser(models.Model):
             response = requests.get(f'https://poshmark.com/closet/{username}{random_int}')
 
         return f'{username}{random_int}'
-
-    @staticmethod
-    def is_english(text):
-        if not text:
-            return False
-        try:
-            text.encode(encoding='utf-8').decode('ascii')
-        except UnicodeDecodeError:
-            return False
-        else:
-            return True
 
     def __str__(self):
         return f'Posh User - Username: {self.username}'
