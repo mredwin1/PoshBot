575599bec7f843a5e2ffdf3a2f3005ec302d9f60
Edwin Cruz <ecruz1113@gmail.com>
2021-07-26 17:02:24 -0400

    Update

--- poshmark/forms.py
+++ poshmark/forms.py
@@ -227,7 +227,7 @@ class CreateCampaign(forms.Form):
 
             self.cleaned_data[times_field] = ','.join(datetimes)
 
-        if self.cleaned_data['mode'] != Campaign.BASIC_SHARING:
+        if self.cleaned_data['mode'] == Campaign.ADVANCED_SHARING:
             listings_field = 'listings'
             if listings_field in self.cleaned_data.keys():
                 listing_ids = self.cleaned_data[listings_field].split(',')
