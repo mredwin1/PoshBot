91bb4a2ffd9c525514de5e31cca77e81b93824d8
Edwin Cruz <ecruz1113@gmail.com>
2021-07-27 21:33:03 -0400

    Update

--- poshmark/chrome_clients/clients.py
+++ poshmark/chrome_clients/clients.py
@@ -281,11 +281,11 @@ class PhoneNumber:
             if verification_response or verification_response.status_code == requests.codes.ok:
                 verification_response_json = verification_response.json()
                 if verification_response_json['state'] == 'WAITING_FOR_SMS':
-                    message = 'Clicked send again, sleeping for 30 seconds' if send_again_element else 'SMS not received, sleeping for 30 seconds'
+                    message = 'Clicked send again, sleeping for 30 seconds' if send_again_element else 'SMS not received, sleeping for 10 seconds'
                     self.logger.info(message)
                     if send_again_element:
                         send_again_element.click()
-                    time.sleep(30)
+                    time.sleep(10)
 
         if verification_response_json['state'] == 'ERROR':
             self.logger.error(verification_response_json['msg'])
@@ -2030,7 +2030,7 @@ class PoshMarkClient(BaseClient):
 
                         self.logger.info(f'Verification code retrieved successfully: {verification_code}')
                         self.logger.info('Marking email for deletion')
-                        imap.store(email_id, "+FLAGS", "\\Deleted")
+                        # imap.store(email_id, "+FLAGS", "\\Deleted")
 
                         return verification_code
                 self.logger.warning('Verification code not ready')
