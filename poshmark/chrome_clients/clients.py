e95f614af93ba8ab8d9be5db7e1e6a4dd47226c5
Edwin Cruz <ecruz1113@gmail.com>
2021-08-28 21:36:26 -0400

    Update

--- poshmark/chrome_clients/clients.py
+++ poshmark/chrome_clients/clients.py
@@ -1665,7 +1665,7 @@ class PoshMarkClient(BaseClient):
                     email_verification_code = None
                     email_verification_code_attempts = 0
                     while not email_verification_code and email_verification_code_attempts < 4:
-                        email_verification_code = PoshUser.get_email_verification_code(self.get_redis_object_attr(self.redis_posh_user_id, 'inbox_id'))
+                        email_verification_code = PoshUser.get_email_verification_code(self.get_redis_object_attr(self.redis_posh_user_id, 'email_id'))
                         if not email_verification_code:
                             self.sleep(60)
                             email_verification_code_attempts += 1
